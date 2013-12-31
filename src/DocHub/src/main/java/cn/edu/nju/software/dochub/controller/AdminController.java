b2c55b63e5418db86ac05b9d447dc98192afd7ba
Yiming Tang <yimingnju@gmail.com>
2013-12-31 15:23:24 +0000

    Use .vm as velocity template extension

--- src/DocHub/src/main/java/cn/edu/nju/software/dochub/controller/AdminController.java
+++ src/DocHub/src/main/java/cn/edu/nju/software/dochub/controller/AdminController.java
@@ -29,207 +29,194 @@ import javax.servlet.http.HttpServletResponse;
 @Controller
 @RequestMapping("/admin")
 public class AdminController {
-	UserService userService;
-	CommentService commentService;
-	ResponseBuilder responseBuilder;
-	DocumentService documentService;
+    UserService userService;
+    CommentService commentService;
+    ResponseBuilder responseBuilder;
+    DocumentService documentService;
 
-	@RequestMapping(value = "/index.html")
-	public String Index(HttpServletRequest request,
-			HttpServletResponse response, ModelMap model) {
-		model.put("userAccessContext", (UserAccessContext) request.getSession()
-				.getAttribute("userAccessContext"));
-		return "admin/index";
-	}
+    @RequestMapping(value = "/index.html")
+    public String Index(HttpServletRequest request,
+                        HttpServletResponse response, ModelMap model) {
+        return "redirect: user.html";
+    }
 
-	@RequestMapping(value = "/tag.html")
-	public String Tag(HttpServletRequest request, HttpServletResponse response,
-			ModelMap model) {
-		return "admin/tag";
-	}
+    @RequestMapping(value = "/tag.html")
+    public String Tag(HttpServletRequest request, HttpServletResponse response,
+                      ModelMap model) {
+        return "admin/tag";
+    }
 
-	@RequestMapping(value = "/document.html")
-	public String Document(HttpServletRequest request,
-			HttpServletResponse response, ModelMap model) {
-		model.put("userAccessContext", (UserAccessContext) request.getSession()
-				.getAttribute("userAccessContext"));
-		model.put("allDocumentList", documentService.getAllDocument());
-		model.put("documentTypeList", documentService.getAllDocumentType());
-		model.put("dateformat",
-				new SimpleDateFormat("yyyy.MM.dd"));
-		return "/admin/document";
-	}
-	
-	@RequestMapping(value = "/user.html")
-	public String User(HttpServletRequest request,
-			HttpServletResponse response, ModelMap model) {
-		model.put("userAccessContext", (UserAccessContext) request.getSession()
-				.getAttribute("userAccessContext"));
-		model.put("userList", userService.getAllUser());
-		return "admin/user";
-	}
-	
-	@RequestMapping(value = "/detailedcomment.html")
-	public String detailedComment(HttpServletRequest request,
-			HttpServletResponse response, ModelMap model) {
-		model.put("userAccessContext", (UserAccessContext) request.getSession()
-				.getAttribute("userAccessContext"));
-		model.put("commentPropertyList", commentService.getAllCommentPropertyTypes());
-		return "admin/detailedcomment";
-	}
+    @RequestMapping(value = "/document.html")
+    public String Document(HttpServletRequest request,
+                           HttpServletResponse response, ModelMap model) {
+        model.put("userAccessContext", (UserAccessContext) request.getSession()
+                .getAttribute("userAccessContext"));
+        model.put("allDocumentList", documentService.getAllDocument());
+        model.put("documentTypeList", documentService.getAllDocumentType());
+        model.put("dateformat",
+                new SimpleDateFormat("yyyy.MM.dd"));
+        return "/admin/document";
+    }
 
-	@RequestMapping(value = "/addUser.html")
-	public String addUser(HttpServletRequest request,
-			HttpServletResponse response, ModelMap model) {
-		User user = new User();
-		user.setUsername(request.getParameter("username"));
-		user.setPassword(request.getParameter("password"));
-		System.out.println(">>>>>>" + request.getParameter("password"));
-		user.setCreatedAt(Calendar.getInstance().getTime());
-		user.setSalt("123");
-		user.setName(request.getParameter("name"));
-		String level = request.getParameter("permissionlevel");
-		System.out.println(level);
-		if (level.equals("管理员")) {
-			user.setPermissionLevel((short) 0);
-		} else {
-			user.setPermissionLevel((short) 1);
-		}
+    @RequestMapping(value = "/user.html")
+    public String User(HttpServletRequest request,
+                       HttpServletResponse response, ModelMap model) {
+        model.put("userAccessContext", (UserAccessContext) request.getSession()
+                .getAttribute("userAccessContext"));
+        model.put("userList", userService.getAllUser());
+        return "admin/user";
+    }
 
-		String active = request.getParameter("state");
-		if (active.equals("active")) {
-			user.setActive(true);
-		} else {
-			user.setActive(false);
-		}
+    @RequestMapping(value = "/detailedcomment.html")
+    public String detailedComment(HttpServletRequest request,
+                                  HttpServletResponse response, ModelMap model) {
+        model.put("userAccessContext", (UserAccessContext) request.getSession()
+                .getAttribute("userAccessContext"));
+        model.put("commentPropertyList", commentService.getAllCommentPropertyTypes());
+        return "admin/detailedcomment";
+    }
 
-		userService.addUser(user);
-		return "forward:user.html";
-	}
+    @RequestMapping(value = "/addUser.html")
+    public String addUser(HttpServletRequest request,
+                          HttpServletResponse response, ModelMap model) {
+        User user = new User();
+        user.setUsername(request.getParameter("username"));
+        user.setPassword(request.getParameter("password"));
+        System.out.println(">>>>>>" + request.getParameter("password"));
+        user.setCreatedAt(Calendar.getInstance().getTime());
+        user.setSalt("123");
+        user.setName(request.getParameter("name"));
+        String level = request.getParameter("permissionlevel");
+        System.out.println(level);
+        if (level.equals("管理员")) {
+            user.setPermissionLevel((short) 0);
+        } else {
+            user.setPermissionLevel((short) 1);
+        }
 
-	@RequestMapping(value = "/editUser.html")
-	public String editUser(HttpServletRequest request,
-			HttpServletResponse response, ModelMap model) {
-		User user = userService.getUserById(Integer.parseInt(request
-				.getParameter("edituserid")));
-		user.setUpdatedAt(Calendar.getInstance().getTime());
-		user.setUsername(request.getParameter("editusername"));
-		user.setPassword(request.getParameter("editpassword"));
-		user.setName(request.getParameter("editname"));
-		String level = request.getParameter("editpermissionlevel");
-		System.out.println(level);
-		if (level.equals("管理员")) {
-			user.setPermissionLevel((short) 0);
-		} else {
-			user.setPermissionLevel((short) 1);
-		}
+        String active = request.getParameter("state");
+        if (active.equals("active")) {
+            user.setActive(true);
+        } else {
+            user.setActive(false);
+        }
 
-		String active = request.getParameter("editstate");
-		if (active.equals("active")) {
-			user.setActive(true);
-		} else {
-			user.setActive(false);
-		}
+        userService.addUser(user);
+        return "forward:user.html";
+    }
 
-		userService.updateUser(user);
-		return "forward:user.html";
-	}
+    @RequestMapping(value = "/editUser.html")
+    public String editUser(HttpServletRequest request,
+                           HttpServletResponse response, ModelMap model) {
+        User user = userService.getUserById(Integer.parseInt(request
+                .getParameter("edituserid")));
+        user.setUpdatedAt(Calendar.getInstance().getTime());
+        user.setUsername(request.getParameter("editusername"));
+        user.setPassword(request.getParameter("editpassword"));
+        user.setName(request.getParameter("editname"));
+        String level = request.getParameter("editpermissionlevel");
+        System.out.println(level);
+        if (level.equals("管理员")) {
+            user.setPermissionLevel((short) 0);
+        } else {
+            user.setPermissionLevel((short) 1);
+        }
 
-	@RequestMapping(value = "/getUser.aj")
-	public void getUserById(HttpServletRequest request,
-			HttpServletResponse response, ModelMap model) {
-		int id = Integer.parseInt(request.getParameter("userid"));
-		User user = userService.getUserById(id);
-		JSONObject json = new JSONObject();
-		json.put("userid", user.getId());
-		json.put("username", user.getUsername());
-		json.put("password", user.getPassword());
-		json.put("name", user.getName());
-		if (user.getPermissionLevel() == 0) {
-			json.put("permissionlevel", "管理员");
-		} else {
-			json.put("permissionlevel", "普通用户");
-		}
-		if (user.getActive()) {
-			json.put("state", "active");
-		} else {
-			json.put("state", "unactive");
-		}
-		responseBuilder.WriteJSONObject(response, json);
-	}
+        String active = request.getParameter("editstate");
+        if (active.equals("active")) {
+            user.setActive(true);
+        } else {
+            user.setActive(false);
+        }
 
-	@RequestMapping(value = "/usernameExist.aj", method = RequestMethod.POST)
-	public void existUser(HttpServletRequest request,
-			HttpServletResponse response, ModelMap model) {
-		User user = userService.getUserByUserName(request
-				.getParameter("username"));
-		boolean exist = true;
-		if (user == null) {
-			exist = false;
-		}
-		JSONObject json = new JSONObject();
-		json.put("exist", exist);
-		responseBuilder.WriteJSONObject(response, json);
-	}
+        userService.updateUser(user);
+        return "forward:user.html";
+    }
 
-	@RequestMapping(value = "/editusernameExist.aj", method = RequestMethod.POST)
-	public void editExistUser(HttpServletRequest request,
-			HttpServletResponse response, ModelMap model) {
-		User user = userService.getUserByUserName(request
-				.getParameter("username"));
-		int userid = Integer.parseInt(request.getParameter("userid"));
-		boolean exist = true;
-		if (user.getId() == userid) {
-			exist = false;
-		}
-		JSONObject json = new JSONObject();
-		json.put("exist", exist);
-		responseBuilder.WriteJSONObject(response, json);
-	}
-	
-	@RequestMapping(value = "/addcommentproperty.html", method = RequestMethod.POST)
-	public String addCommentProperty(HttpServletRequest request,
-			HttpServletResponse response, ModelMap model) {
-		String commentproperty = request.getParameter("commentproperty");
-		CommentPropertyType cpt=new CommentPropertyType(commentproperty);
-		commentService.addCommentPropertyType(cpt);
-		return "redirect:/admin/detailedcomment.html";
-	}
-	
-	@RequestMapping(value = "/deletecommentproperty.html", method = RequestMethod.GET)
-	public String deleteCommentProperty(HttpServletRequest request,
-			HttpServletResponse response, ModelMap model) {
-		int id=Integer.parseInt(request.getParameter("commentpropetyid"));
-		commentService.deleteCommentPropertyType(id);
-		return "redirect:/admin/detailedcomment.html";
-	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+    @RequestMapping(value = "/getUser.aj")
+    public void getUserById(HttpServletRequest request,
+                            HttpServletResponse response, ModelMap model) {
+        int id = Integer.parseInt(request.getParameter("userid"));
+        User user = userService.getUserById(id);
+        JSONObject json = new JSONObject();
+        json.put("userid", user.getId());
+        json.put("username", user.getUsername());
+        json.put("password", user.getPassword());
+        json.put("name", user.getName());
+        if (user.getPermissionLevel() == 0) {
+            json.put("permissionlevel", "管理员");
+        } else {
+            json.put("permissionlevel", "普通用户");
+        }
+        if (user.getActive()) {
+            json.put("state", "active");
+        } else {
+            json.put("state", "unactive");
+        }
+        responseBuilder.WriteJSONObject(response, json);
+    }
 
-	public void setUserService(UserService userService) {
-		this.userService = userService;
-	}
+    @RequestMapping(value = "/usernameExist.aj", method = RequestMethod.POST)
+    public void existUser(HttpServletRequest request,
+                          HttpServletResponse response, ModelMap model) {
+        User user = userService.getUserByUserName(request
+                .getParameter("username"));
+        boolean exist = true;
+        if (user == null) {
+            exist = false;
+        }
+        JSONObject json = new JSONObject();
+        json.put("exist", exist);
+        responseBuilder.WriteJSONObject(response, json);
+    }
 
-	public void setResponseBuilder(ResponseBuilder responseBuilder) {
-		this.responseBuilder = responseBuilder;
-	}
+    @RequestMapping(value = "/editusernameExist.aj", method = RequestMethod.POST)
+    public void editExistUser(HttpServletRequest request,
+                              HttpServletResponse response, ModelMap model) {
+        User user = userService.getUserByUserName(request
+                .getParameter("username"));
+        int userid = Integer.parseInt(request.getParameter("userid"));
+        boolean exist = true;
+        if (user.getId() == userid) {
+            exist = false;
+        }
+        JSONObject json = new JSONObject();
+        json.put("exist", exist);
+        responseBuilder.WriteJSONObject(response, json);
+    }
 
-	public void setCommentService(CommentService commentService) {
-		this.commentService = commentService;
-	}
+    @RequestMapping(value = "/addcommentproperty.html", method = RequestMethod.POST)
+    public String addCommentProperty(HttpServletRequest request,
+                                     HttpServletResponse response, ModelMap model) {
+        String commentproperty = request.getParameter("commentproperty");
+        CommentPropertyType cpt = new CommentPropertyType(commentproperty);
+        commentService.addCommentPropertyType(cpt);
+        return "redirect:/admin/detailedcomment.html";
+    }
 
-	public void setDocumentService(DocumentService documentService) {
-		this.documentService = documentService;
-	}
+    @RequestMapping(value = "/deletecommentproperty.html", method = RequestMethod.GET)
+    public String deleteCommentProperty(HttpServletRequest request,
+                                        HttpServletResponse response, ModelMap model) {
+        int id = Integer.parseInt(request.getParameter("commentpropetyid"));
+        commentService.deleteCommentPropertyType(id);
+        return "redirect:/admin/detailedcomment.html";
+    }
+
+
+    public void setUserService(UserService userService) {
+        this.userService = userService;
+    }
+
+    public void setResponseBuilder(ResponseBuilder responseBuilder) {
+        this.responseBuilder = responseBuilder;
+    }
+
+    public void setCommentService(CommentService commentService) {
+        this.commentService = commentService;
+    }
+
+    public void setDocumentService(DocumentService documentService) {
+        this.documentService = documentService;
+    }
 
 }
